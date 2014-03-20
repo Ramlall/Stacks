d54bc5a5a76421e616195d66975ab3bd4f47ea92
Blueobelisk <tariqramlall@gmail.com>
2014-03-20 02:37:42 -0400

    First commit.

---
+++ Stacks/OpEval.cs
@@ -0,0 +1,88 @@
+﻿public class OpEval
+	{
+	///////////////////////
+	private int GetOp(int ch)
+		{
+		if(ch=='.') { return 0; }
+		if(ch=='+') { return 1; }
+		if(ch=='*') { return 2; }
+		if(ch=='$') { return 0; }
+		System.Console.WriteLine("**Get OP Got a bad oeprator");
+		return -1; 
+		}	
+
+	///////////////////////
+	public int Eval(string str)
+		{
+		Stack opstack=new Stack(300);
+		Stack numstack=new Stack(300);
+		
+
+		opstack.Push('.');
+		for(int i=0;i<str.Length;++i)
+			{
+			char ch=str[i];
+			if(ch>='0' && ch<='9')
+				{
+				numstack.Push(ch-'0');
+				continue;
+				}
+
+
+			for(;;)
+				{
+				if(GetOp(opstack.GetTop() ) < GetOp(ch) )
+					{
+					opstack.Push(ch);
+					break; 
+					}
+
+				int performop=opstack.GetTop();
+				if(performop=='+')
+					{
+					int num1=numstack.GetTop();
+					numstack.Pop();
+					int num2=numstack.GetTop();
+					numstack.Pop();
+					numstack.Push(num1+num2);
+					opstack.Pop();
+					continue;
+					}
+
+
+				if(performop=='*')
+					{
+					int num1=numstack.GetTop();
+					numstack.Pop();
+					int num2=numstack.GetTop();
+					numstack.Pop();
+					numstack.Push(num1*num2);
+					opstack.Pop();
+					continue;
+					}
+
+				if(performop=='.')
+					{
+					return numstack.Pop();
+					}
+
+				//=Error
+				System.Console.WriteLine("** Stack Processing Error");
+				}
+
+			}
+
+		System.Console.WriteLine("** Error in Eval()");
+		return 0;
+		}
+
+
+	//////////////////////////////////////////////////////////////
+	static public void Main()
+		{
+		OpEval tst=new OpEval();
+		int num=tst.Eval("2+3*3+5$");
+		System.Console.WriteLine("Answer={0}",num);
+		System.Console.ReadKey();
+		}
+	}
---
+++ Stacks/Stack.cs
@@ -0,0 +1,74 @@
+﻿//The Stack functions Prof Devi taught Joe and I in class.
+public class Stack
+{
+	private int[] mArray;
+	private int mTop;
+
+	//Constructor 
+	public Stack(int maxCap)
+	{
+		mTop = -1;
+		mArray = new int[maxCap];
+	}
+
+	//Destructor = something special when something gets deleted.
+
+	~Stack()
+	{
+		for (int i = 0; i < mArray.Length; i++)
+		{
+			mArray[i] = 0;
+		}
+	}
+
+	public void Push(int val)
+	{
+		if (mTop >= mArray.Length - 1)
+		{
+			System.Console.WriteLine("Stack is full.");
+			return;
+		}
+		mTop = mTop + 1;
+		mArray[mTop] = val;
+	}
+
+	public int Pop()
+	{
+		if (mTop < 0)
+		{
+			System.Console.WriteLine("Stack is empty");
+			return 0;
+		}
+		int val = mArray[mTop];
+		mTop = mTop - 1;
+		return val;
+	}
+
+	public int GetTop()
+	{
+		if (mTop < 0)
+		{
+			System.Console.WriteLine("Stack if Empty");
+			return 0;
+		}
+		return mArray[mTop];
+	}
+
+	public bool IsEmpty()
+	{
+		if (mTop < 0)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public bool IsFull()
+	{
+		if (mTop >= mArray.Length - 1)
+		{
+			return true;
+		}
+		return false;
+	}
+}
